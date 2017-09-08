a4495a5742246d489e0631aca00c6cfcc32768d8
ruguo@thoughtworks.com <18792561236@163.com>
2017-09-08 23:42:36 +0800

    [RU] | use shell write a html file(easy-html.sh)

---
+++ shell_test/easy-html.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cat <<- _EOF_
+    <html>
+    <head>
+        <title>
+        My System Information
+        </title>
+    </head>
+
+    <body>
+    <h1>My System Information</h1>
+    </body>
+    </html>
+_EOF_
