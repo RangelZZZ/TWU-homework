63bb90a3a4790f38625a4cc036c8c27a2b6f6834
ruguo@thoughtworks.com <18792561236@163.com>
2017-09-12 23:05:44 +0800

    [RU] - finished triangle-exercises oneAsterisk

--- triangle-exercises/src/main/OneAsterisk.java
+++ triangle-exercises/src/main/OneAsterisk.java
@@ -2,5 +2,8 @@ package main;
 
 public class OneAsterisk {
 
+  public char printAsterisk(){
+    return '*';
+  };
 }
 
--- triangle-exercises/src/test/OneAsteriskTest.java
+++ triangle-exercises/src/test/OneAsteriskTest.java
@@ -1,5 +1,17 @@
 package test;
 
+import main.OneAsterisk;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
 public class OneAsteriskTest {
-    
+     private OneAsterisk oneAsterisk =  new OneAsterisk();
+
+    @Test
+    public void should_return_asterisk() throws Exception {
+        assertThat(oneAsterisk.printAsterisk(),is('*'));
+    }
+
 }
