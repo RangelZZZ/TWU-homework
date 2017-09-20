080292c3aa6d71ac56f68d6ea666ad0a880c44fc
ruguo@thoughtworks.com <18792561236@163.com>
2017-09-20 22:51:33 +0800

    [Ru] | finished core layer checkout Movie

--- TWU_Biblioteca-master/test/core/movieListTest.java
+++ TWU_Biblioteca-master/test/core/movieListTest.java
@@ -3,6 +3,7 @@ package core;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 public class movieListTest {
@@ -14,5 +15,19 @@ public class movieListTest {
 
         assertThat(movieList.getMovieList().get(0).getYear(), is("2001"));
         assertThat(movieList.getMovieList().size(), is(3));
+    }
+
+    @Test
+    public void should_checkout_movie_when_the_movie_is_exist() throws Exception {
+        movieList.checkOutMovie("movie1");
+        Movie movie = movieList.getMovieList().stream().filter(s -> (!s.getAvailable() && s.getName().equals("movie1"))).findFirst().orElse(null);
+        assertThat(movie.getDirector(),is("director1"));
+    }
+
+    @Test
+    public void should_not_checkout_book_when_the_book_is_not_exist() throws Exception {
+        movieList.checkOutMovie("book4");
+        Movie movie = movieList.getMovieList().stream().filter(s -> (!s.getAvailable() && s.getName().equals("book4"))).findFirst().orElse(null);
+        assertNull(movie);
     }
 }
